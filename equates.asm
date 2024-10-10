e8ef34c648f86789622d5a7664375ab45e65c174
MoparStephen <moparstephen@gmail.com>
2024-10-10 14:14:05 -0400

    Added more equates

---
+++ equates.asm
@@ -0,0 +1,146 @@
+; OS Vectors
+POKMSK      equ $10
+RTCLOK      equ $12
+CRITIC      equ $42
+ATRACT      equ $4D
+LMARGIN     equ $52
+RMARGIN     equ $52
+ROWCRS      equ $54
+COLCRS      equ $55         ; $55-$56
+DINDEX      equ $57         ; Display Mode
+SAVMSC      equ $58         ; $58-$59
+; $D4-$FF reserved for Floating Point routines
+FR0         equ $D4         ; $D4-$D9 6-byte BCD Floating Point Register 0
+; $DA-$DF free?
+FR1         equ $E0         ; $E0-$E5 6-byte BCD Floating Point Register 1
+FR2         equ $E6         ; $E6-$EB 6-byte BCD Floating Point Register 2
+FRX         equ $EC         ; Spare FP register
+EEXP        equ $ED         ; Value of exponent
+NSIGN       equ $EE         ; Sign of the Floating Point number
+ESIGN       equ $EF         ; Sign of the exponent
+FCHRFLG     equ $F0         ; First character flag
+DIGRT       equ $F1         ; Number of digits to right of decimal
+CIX         equ $F2         ; Character Input indeX - offset into INBUFF
+INBUFF      equ $F3         ; $F3-$F4 Input ASCII pointer.  Result output buffer is $580-$5FF
+ZTEMP1      equ $F5         ; $F5-$F6 Temp register
+ZTEMP4      equ $F7         ; $F7-$F8 Temp register
+ZTEMP3      equ $F9         ; $F9-$FA Temp register
+RADFLG      equ $FB         ; When 0 use Radians (default OS value) else Degrees
+FLPTR       equ $FC         ; $FC-$FD Pointer to user's Floating Point number 1
+FPTR2       equ $FE         ; $FE-$FF Pointer to user's Floating Point number 2
+; $0100-$01FF Stack
+VDSLST      equ $0200       ; Vector to Display List Interrupt routine (Word)
+VSERIN      equ $020A
+SDMCTL      equ $022F
+SDLSTL      equ $0230       ; Word
+STRIG0      equ $0284       ; 0 = Pressed, 1 = Released
+STRIG1      equ $0285       ; 0 = Pressed, 1 = Released
+STRIG2      equ $0286       ; 0 = Pressed, 1 = Released
+STRIG3      equ $0287       ; 0 = Pressed, 1 = Released
+TXTROW      equ $0290
+TXTCOL      equ $0291       ; Word
+PCOLR0      equ $02C0
+PCOLR1      equ $02C1
+PCOLR2      equ $02C2
+PCOLR3      equ $02C3
+COLOR2      equ $02C6       ; Playfield in GR.0
+COLOR4      equ $02C8       ; Border    in GR.0
+CRSINH      equ $02F0       ; Cursor Inhibit (0 = On)
+CH          equ $02FC       ; Last Key Pressed
+
+/*
+ICHID       equ $0340       ; Index into device name
+ICDNO       equ $0341       ; Device #
+ICCOM       equ $0342       ; Command (also ICCMD)
+ICSTA       equ $0343       ; Most recent device status
+ICBAL       equ $0344       ; Buffer for data transfer or address of filename for OPEN
+ICBAH       equ $0345       ; Buffer for data transfer or address of filename for OPEN
+ICPTL       equ $0346       ; Address of PUT-one-byte routine minus one
+ICPTH       equ $0347       ; Address of PUT-one-byte routine minus one
+ICBLL       equ $0348       ; Buffer length, max # bytes to PUT or GET
+ICBLH       equ $0349       ; Buffer length, max # bytes to PUT or GET
+ICAX1       equ $034A       ; Auxiliary Byte 1
+ICAX2       equ $034B       ; Auxiliary Byte 2
+ICAX3       equ $034C       ; Auxiliary Byte 3
+ICAX4       equ $034D       ; Auxiliary Byte 4
+ICAX5       equ $034E       ; Auxiliary Byte 5
+ICAX6       equ $034F       ; Auxiliary Byte 6
+
+$0340-$034F: IOCB For Channel 0
+$0350-$035F: IOCB For Channel 1
+$0360-$036F: IOCB For Channel 2
+$0370-$037F: IOCB For Channel 3
+$0380-$038F: IOCB For Channel 4
+$0390-$039F: IOCB For Channel 5
+$03A0-$03AF: IOCB For Channel 6
+$03B0-$03BF: IOCB For Channel 7
+*/
+
+LINBUFF     equ $0580       ; FP ASCII output buffer, $580-$5FF
+
+;-----------------------------------------------------------------------------
+;  HARDWARE EQUATES
+;-----------------------------------------------------------------------------
+; GTIA (D000-D0FF)
+HPOSP0      equ $D000
+HPOSP1      equ $D001
+HPOSP2      equ $D002
+HPOSP3      equ $D003
+SIZEP0      equ $D008
+SIZEP1      equ $D009
+SIZEP2      equ $D00A
+SIZEP3      equ $D00B
+GRAFP0      equ $D00D
+COLBAK      equ $D01A
+PRIOR       equ $D01B
+GRACTL      equ $D01D
+CONSOL      equ $D01F
+
+; POKEY (D200-D2FF)
+KBCODE      equ $D209
+SKSTAT      equ $D20F
+
+; PIA (D300-D3FF)
+PORTA       equ $D300
+PORTB       equ $D301
+PACTL       equ $D302
+PBCTL       equ $D303
+; $D304-$D3FF continually repeat $D300-$D304
+
+; ANTIC (D400-D5FF)
+DMACTL      equ $D400
+PMBASE      equ $D407
+WSYNC       equ $D40A
+VCOUNT      equ $D40B
+NMIEN       equ $D40E
+NMIST       equ $D40F
+
+; FP ROM ($D800-$DFFF)
+AFP         equ $D800       ; ASCII to FP
+FASC        equ $D8E6       ; FP to ASCII
+IFP         equ $D9AA       ; integer to FP
+FPI         equ $D9D2       ; FP to integer
+ZFR0        equ $DA44       ; clear FR0
+ZF1         equ $DA46       ; clear zero page FP buffer
+FSUB        equ $DA60       ; FR0 - FR1
+FADD        equ $DA66       ; FR0 + FR1
+FMUL        equ $DADB       ; FR0 * FR1
+FDIV        equ $DB28       ; FR0 / FR1
+PLYEVL      equ $DD40       ; polynomial evaluation
+FLD0R       equ $DD89       ; load FR0 by X,Y register pointer
+FLD0P       equ $DD8D       ; load FR0 by FLPTR pointer
+FLD1R       equ $DD98       ; load FR1 by X,Y register pointer
+FLD1P       equ $DD9C       ; load FR1 by FLPTR pointer
+FST0R       equ $DDA7       ; store FR0 at buffer by X,Y register pointer
+FST1P       equ $DDAB       ; store FR0 at buffer by FLPTR pointer
+FMOVE       equ $DDB6       ; move FR0 to FR1
+EXP         equ $DDC0       ; e exponentiation
+EXP10       equ $DDCC       ; base 10 exponentiation
+LOG         equ $DECD       ; natural log of FR0
+LOG10       equ $DED1       ; base 10 log of FR0
+
+; ROM Vectors
+;CIOV        equ $E456
+SETVBV      equ $E45C       ; Set system timers and interrupt vectors
+SYSVBV      equ $E45F       ; Stage one VBLANK calculations entry point
+XITVBV      equ $E462       ; Exit VBLANK routine entry point
